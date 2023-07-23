5bd822f593845e4e80fe0ea7aa45afb904d8aebb
ByteAadi <139967362+ByteAadi@users.noreply.github.com>
2023-07-23 15:53:30 +0530

    Add files via upload

---
+++ routes/orders.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const authorization=require("../middleware/postmiddleware")
+const Router = express.Router()
+const OrderModel = require("../model/orders");
+
+Router.post("/new",authorization,async(req,res)=>{
+    try {
+        const {quantity,totalprice,products,address,Arriving,email,status}=req.body
+        if(!quantity||!totalprice||!products||!address||!Arriving){
+            return req.status(400).json({msg:"please provide all the order details"})
+        }
+        const DatatoDb= new OrderModel({quantity,totalprice,products,address,Arriving,email,status})
+            await DatatoDb.save()
+            return res.status(200).json({msg:"order completed"})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({msg:"Internal server error"})
+    }
+
+})
+Router.get("/details",authorization,async(req,res)=>{
+    try {
+       const{email}=req.body
+       
+        const DatafromDb=await OrderModel.find({email})
+           
+            return res.status(200).json(DatafromDb)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({msg:"Internal server error"})
+    }
+
+})
+Router.get("/one/:id",authorization,async(req,res)=>{
+    try {
+       const{id}=req.params
+       
+        const DatafromDb=await OrderModel.findByid(id)
+           
+            return res.status(200).json(DatafromDb)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({msg:"Internal server error"})
+    }
+
+})
+Router.delete("/cancel/:id",authorization,async(req,res)=>{
+    try {
+       const{id}=req.params
+       const status=req.body.status
+        const DatafromDb=await OrderModel.findByid(id)
+           DatafromDb.status=status
+           await DatafromDb.save()
+            return res.status(200).json({msg:"orderd canceld"})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({msg:"Internal server error"})
+    }
+
+})
+module.exports=Router
