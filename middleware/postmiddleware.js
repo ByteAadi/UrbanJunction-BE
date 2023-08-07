8439b676269e45f6a92aa305c6708e0cc0fe66b8
ByteAadi <139967362+ByteAadi@users.noreply.github.com>
2023-08-07 20:35:57 +0530

    Update postmiddleware.js

--- middleware/postmiddleware.js
+++ middleware/postmiddleware.js
@@ -4,7 +4,7 @@ const jwt=require("jsonwebtoken")
 const Validate=(req,res,next)=>{
     try {
         const token=req.headers.token
-        console.log(req.body.token)
+        console.log(req)
         console.log(req.headers)
        console.log(token)
         if(token){
